0c58f34235ab375f8e8b67602a7e4e1109d4de78
Bujar Hyseni <buj4r.hyseni@gmail.com>
2021-12-03 00:17:07 +0100

    Firebase api changed for prod

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAjxRwrpBcmiiRN38Ry3tgsa8PfTdIdCbA",
+  authDomain: "auth-development-cb65b.firebaseapp.com",
+  databaseURL: "https://auth-development-cb65b-default-rtdb.firebaseio.com",
+  projectId: "auth-development-cb65b",
+  storageBucket: "auth-development-cb65b.appspot.com",
+  messagingSenderId: "1066542541235",
+  appId: "1:1066542541235:web:490b2ebe68d0b8152e03c6"
 });
 
 export const auth = app.auth();
