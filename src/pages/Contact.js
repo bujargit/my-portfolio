ce4960bad6c257ea6d25124b8d65648dbc9e89e3
Bujar Hyseni <buj4r.hyseni@gmail.com>
2022-01-07 00:44:33 +0100

    Update new portfolio

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import * as emailjs from "emailjs-com";
 import "./Contact.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { Container, Row, Col, Alert } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { meta } from "../content_option";
 import { contactConfig } from "../content_option";
 import { introdata } from "../content_option";
@@ -18,52 +18,52 @@ export default function ContactUs() {
     variant: "",
   });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setFormdata({ loading: true });
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setFormdata({ loading: true });
 
-    const templateParams = {
-      from_name: formData.email,
-      user_name: formData.name,
-      to_name: contactConfig.YOUR_EMAIL,
-      message: formData.message,
-    };
+  //   const templateParams = {
+  //     from_name: formData.email,
+  //     user_name: formData.name,
+  //     to_name: contactConfig.YOUR_EMAIL,
+  //     message: formData.message,
+  //   };
 
-    emailjs
-      .send(
-        contactConfig.YOUR_SERVICE_ID,
-        contactConfig.YOUR_TEMPLATE_ID,
-        templateParams,
-        contactConfig.YOUR_USER_ID
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setFormdata({
-            loading: false,
-            alertmessage: "SUCCESS! ,Thankyou for your messege",
-            variant: "success",
-            show: true,
-          });
-        },
-        (error) => {
-          console.log(error.text);
-          setFormdata({
-            alertmessage: `Faild to send!,${error.text}`,
-            variant: "danger",
-            show: true,
-          });
-          document.getElementsByClassName("co_alert")[0].scrollIntoView();
-        }
-      );
-  };
+  //   emailjs
+  //     .send(
+  //       contactConfig.YOUR_SERVICE_ID,
+  //       contactConfig.YOUR_TEMPLATE_ID,
+  //       templateParams,
+  //       contactConfig.YOUR_USER_ID
+  //     )
+  //     .then(
+  //       (result) => {
+  //         console.log(result.text);
+  //         setFormdata({
+  //           loading: false,
+  //           alertmessage: "SUCCESS! ,Thankyou for your messege",
+  //           variant: "success",
+  //           show: true,
+  //         });
+  //       },
+  //       (error) => {
+  //         console.log(error.text);
+  //         setFormdata({
+  //           alertmessage: `Faild to send!,${error.text}`,
+  //           variant: "danger",
+  //           show: true,
+  //         });
+  //         document.getElementsByClassName("co_alert")[0].scrollIntoView();
+  //       }
+  //     );
+  // };
 
-  const handleChange = (e) => {
-    setFormdata({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
+  // const handleChange = (e) => {
+  //   setFormdata({
+  //     ...formData,
+  //     [e.target.name]: e.target.value,
+  //   });
+  // };
 
   return (
     <HelmetProvider>
